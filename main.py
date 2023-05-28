0d39e4a579ab7f37d3bf9042d700adfafbe77b13
Pratham34 <99575732+Pratham34@users.noreply.github.com>
2023-05-28 22:46:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://www.iplt20.com/auction/2022#:~:text=TATA%20IPL%20Auction%20%2D%202022,IPL)%202022%20Auction%20in%20Bengaluru."
+r = requests.get(url)
+# print(r)
+# 200 response means we can fetch its HTML
+
+soup = BeautifulSoup(r.text,"lxml")
+# print(soup)
+
+table = soup.find("table",class_="ih-td-tab auction-tbl")
+# print(table)
+title = table.find_all("th",)
+# print(title)
+
+header = []
+for i in title:
+    name = i.text
+    header.append(name)
+
+# print(header)
+
+df = pd.DataFrame(columns=header)
+print(df)
+
+rows = table.find_all("tr")
+# print(rows)
+for i in rows[1:]:
+    first_td = i.find_all("td")[0].find("div",class_="ih-pt-ic").text.strip()
+    data = i.find_all("td")[1:]
+    row = [tr.text for tr in data]
+    # print(row)
+    row.insert(0,first_td)
+    l = len(df)
+    df.loc[l] = row  
+    print(data)    
+
+print(df)
+df.to_csv("ipl Auction stats.csv")
